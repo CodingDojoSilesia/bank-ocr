ae36d4e732bcd10ff1a4b49df7df7d0e79d29251
Michal Klich <michal@michalklich.com>
2019-03-14 23:32:47 +0100

    Lot of changes
    What a shame I did not make a commit earlier.
    I decided to change how initialisation is done. I have added `from_text`
    classmethod dedicated to parsing text into digits. Internal container for
    digits now can be a list of objects instead of three lists of strings.
    It makes implementation much simpler. All other bits are working fine as
    other objects have not change only their storage.

--- ocr.py
+++ ocr.py
@@ -11,27 +11,33 @@ class UnrecognisedDigit(Exception):
 class Number:
     "Container translating input into individual characters"
 
-    def __init__(self, input_):
-        self.one, self.two, self.three, _ = input_.split(linesep)
+    # change this into from_text
+    @classmethod
+    def from_text(cls, text):
+
+        # FIXME: rename to from_text
+        # method should parse incoming text as __init__ does now
+        # and use digit factory to create a list of digits
+        # __init__ should accept list of digits
+        # iteration and slicing will be much simpler as will be performed
+        # on an actual internal list
+        one, two, three, _ = text.split(linesep)
+        digits = [DigitFactory((one[x*3: (x+1)*3],
+                                two[x*3: (x+1)*3],
+                                three[x*3: (x+1)*3])) for x in range(0, 9)]
+        return Number(digits)
+
+    def __init__(self, digits):
+        self._number = digits
 
     def __getitem__(self, key):
-        if key > 8:
-            raise IndexError
-
-        try:
-            k = key * 3
-            j = k + 3
-        except TypeError:  # we have slice
-            k = key.start * 3
-            j = key.stop * 3
-
-        return DigitFactory((self.one[k:j], self.two[k:j], self.three[k:j]))
+        return self._number[key]
 
     def __str__(self):
-        return "".join(str(d) for d in self)
+        return "".join(str(d) for d in self._number)
 
     def __len__(self):
-        return 9
+        return len(self._number)
 
     def has_invalid_checksum(self):
         try:
@@ -45,28 +51,57 @@ class Number:
     def not_correctly(self):
         return any([isinstance(d, Unknown) for d in self])
 
+    def possible_numbers(self):
+        p = []
+        for i, d in enumerate(self):
+            for f in d.flips():
+                try:
+                    c = list(self)
+                    c[i] = f
+                    new = Number.from_list(c)
+                except IndexError:
+                    new = []
+                p.append(new)
+        # create list of possible no (PN)
+        # FIXME: iterate over digits
+        # get all other options for each digit
+        # append to list of PN
+        # return list of PN
+        return p
+
+
+# This might be for fixing incorrectly scanned numbers
+# allowed_combinations = ["   ", " _ ", "  |", "| |", " _|", "|_ ", "|_|"]
+# [0 elements, 1 element, 2 elements, 3 elements]
+# count elements in current frame see if moving left or right will fix it
+
+# this might be for finding valid checksums
+# another approach is to figure out possible transitions
+# 9 can only be 8 or vice versa
+# 6 can only be 8 and vice versa
+
 
 class DigitFactory:
     def __new__(cls, text):
-        if text == (" _ ", "| |", "|_|"):
-            return Zero(text)
-        if text == ("   ", "  |", "  |"):
-            return One(text)
-        if text == (" _ ", " _|", "|_ "):
+        if text == (" _ ", "| |", "|_|") or text == "0":
+            return Zero((" _ ", "| |", "|_|"))
+        if text == ("   ", "  |", "  |") or text == "1":
+            return One(("   ", "  |", "  |"))
+        if text == (" _ ", " _|", "|_ ") or text == "2":
             return Two(text)
-        if text == (" _ ", " _|", " _|"):
+        if text == (" _ ", " _|", " _|") or text == "3":
             return Three(text)
-        if text == ("   ", "|_|", "  |"):
+        if text == ("   ", "|_|", "  |") or text == "4":
             return Four(text)
-        if text == (" _ ", "|_ ", " _|"):
+        if text == (" _ ", "|_ ", " _|") or text == "5":
             return Five(text)
-        if text == (" _ ", "|_ ", "|_|"):
+        if text == (" _ ", "|_ ", "|_|") or text == "6":
             return Six(text)
-        if text == (" _ ", "  |", "  |"):
-            return Seven(text)
-        if text == (" _ ", "|_|", "|_|"):
-            return Eight(text)
-        if text == (" _ ", "|_|", " _|"):
+        if text == (" _ ", "  |", "  |") or text == "7":
+            return Seven((" _ ", "  |", "  |"))
+        if text == (" _ ", "|_|", "|_|") or text == "8":
+            return Eight((" _ ", "|_|", "|_|"))
+        if text == (" _ ", "|_|", " _|") or text == "9":
             return Nine(text)
 
         return Unknown(text)
@@ -85,8 +120,11 @@ class Digit:
         except ValueError:
             raise UnrecognisedDigit
 
-    def __repr__(self):
-        return linesep.join(self._text) + linesep
+    # def __repr__(self):
+    #     return linesep.join(self._text) + linesep
+
+    def flips(self):
+        return [DigitFactory(o) for o in self._options]
 
 
 class Unknown(Digit):
@@ -95,10 +133,12 @@ class Unknown(Digit):
 
 class Zero(Digit):
     value = "0"
+    _options = ["8"]
 
 
 class One(Digit):
     value = "1"
+    _options = ["7"]
 
 
 class Two(Digit):
@@ -107,6 +147,7 @@ class Two(Digit):
 
 class Three(Digit):
     value = "3"
+    _options = ["9"]
 
 
 class Four(Digit):
@@ -115,35 +156,55 @@ class Four(Digit):
 
 class Five(Digit):
     value = "5"
+    _options = ["9"]
 
 
 class Six(Digit):
     value = "6"
+    _options = ["8", "5"]
 
 
 class Seven(Digit):
     value = "7"
+    _options = ["1"]
 
 
 class Eight(Digit):
     value = "8"
+    _options = ["0"]
 
 
 class Nine(Digit):
     value = "9"
+    _options = ["8", "3"]
 
 
 def scan(text):
-    scanned = Number(text)
+    scanned = Number.from_text(text)
     return str(scanned)
 
 
 def validated_scan(text):
-    scanned = Number(text)
+    scanned = Number.from_text(text)
     if scanned.not_correctly():
         return f"{scanned} ILL"
 
     if scanned.has_invalid_checksum():
         return f"{scanned} ERR"
+
+    return str(scanned)
+
+
+def guessed_scan(text):
+    scanned = Number.from_text(text)
+    if scanned.not_correctly():
+        return f"{scanned} ILL"
+
+    if scanned.has_invalid_checksum():
+        # get all possible numbers
+        return [n for n in scanned.possible_numbers() if not n.has_invalid_checksum()]
+        # iterate and see if any has valid checksum
+        # return successes
+        # return f"{scanned} ERR"
 
     return str(scanned)
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from ocr import scan, validated_scan
+from ocr import scan, validated_scan, guessed_scan
 
 
 def test_input_0s():
@@ -111,7 +111,7 @@ def test_input_123456789():
     assert scan(input_) == "123456789"
 
 
-def test_input_000000051():
+def test_validation_000000051():
     input_ = (
         " _  _  _  _  _  _  _  _    \n"
         "| || || || || || || ||_   |\n"
@@ -121,7 +121,7 @@ def test_input_000000051():
     assert validated_scan(input_) == "000000051"
 
 
-def test_input_49006771_ill():
+def test_validation_49006771_ill():
     input_ = (
         "    _  _  _  _  _  _     _ \n"
         "|_||_|| || ||_   |  |  | _ \n"
@@ -131,7 +131,7 @@ def test_input_49006771_ill():
     assert validated_scan(input_) == "49006771? ILL"
 
 
-def test_input_12345678_ill():
+def test_validation_12345678_ill():
     input_ = (
         "    _  _     _  _  _  _  _ \n"
         "  | _| _||_| _ |_   ||_||_|\n"
@@ -141,7 +141,7 @@ def test_input_12345678_ill():
     assert validated_scan(input_) == "1234?678? ILL"
 
 
-def test_input_664371495_err():
+def test_validation_664371495_err():
     input_ = (
         " _  _     _  _        _  _ \n"
         "|_ |_ |_| _|  |  ||_||_||_ \n"
@@ -149,6 +149,126 @@ def test_input_664371495_err():
         "                           "
     )
     assert validated_scan(input_) == "664371495 ERR"
+
+
+def test_guessed_111111111():
+    input_ = (
+        "                           \n"
+        "  |  |  |  |  |  |  |  |  |\n"
+        "  |  |  |  |  |  |  |  |  |\n"
+        "                           "
+    )
+    assert guessed_scan(input_) == "711111111"
+
+
+def test_guessed_777777777():
+    input_ = (
+        " _  _  _  _  _  _  _  _  _ \n"
+        "  |  |  |  |  |  |  |  |  |\n"
+        "  |  |  |  |  |  |  |  |  |\n"
+        "                           "
+    )
+    assert guessed_scan(input_) == "777777177"
+
+
+def test_guessed_200000000():
+    input_ = (
+        " _  _  _  _  _  _  _  _  _ \n"
+        " _|| || || || || || || || |\n"
+        "|_ |_||_||_||_||_||_||_||_|\n"
+        "                           "
+    )
+    assert guessed_scan(input_) == "200800000"
+
+
+def test_guessed_333333333():
+    input_ = (
+        " _  _  _  _  _  _  _  _  _ \n"
+        " _| _| _| _| _| _| _| _| _|\n"
+        " _| _| _| _| _| _| _| _| _|\n"
+        "                          "
+    )
+    assert guessed_scan(input_) == "333393333"
+
+
+def test_guessed_888888888():
+    input_ = (
+        " _  _  _  _  _  _  _  _  _ \n"
+        "|_||_||_||_||_||_||_||_||_|\n"
+        "|_||_||_||_||_||_||_||_||_|\n"
+        "                           "
+    )
+    assert guessed_scan(input_) == "888888888 AMB ['888886888', '888888880', '888888988']"
+
+
+def test_guessed_555555555():
+    input_ = (
+        " _  _  _  _  _  _  _  _  _ \n"
+        "|_ |_ |_ |_ |_ |_ |_ |_ |_ \n"
+        " _| _| _| _| _| _| _| _| _|\n"
+        "                           "
+    )
+    assert guessed_scan(input_) == "555555555 AMB ['555655555', '559555555']"
+
+
+def test_guessed_666666666():
+    input_ = (
+        " _  _  _  _  _  _  _  _  _ \n"
+        "|_ |_ |_ |_ |_ |_ |_ |_ |_ \n"
+        "|_||_||_||_||_||_||_||_||_|\n"
+        "                           "
+    )
+    assert guessed_scan(input_) == "666666666 AMB ['666566666', '686666666']"
+
+
+def test_guessed_999999999():
+    input_ = (
+        " _  _  _  _  _  _  _  _  _ \n"
+        "|_||_||_||_||_||_||_||_||_|\n"
+        " _| _| _| _| _| _| _| _| _|\n"
+        "                           "
+    )
+    assert guessed_scan(input_) == "999999999 AMB ['899999999', '993999999', '999959999']"
+
+
+def test_guessed_490067715():
+    input_ = (
+        "    _  _  _  _  _  _     _ \n"
+        "|_||_|| || ||_   |  |  ||_ \n"
+        "  | _||_||_||_|  |  |  | _|\n"
+        "                           "
+    )
+    assert guessed_scan(input_) == "490067715 AMB ['490067115', '490067719', '490867715']"
+
+
+def test_guessed_123456789():
+    input_ = (
+        "    _  _     _  _  _  _  _ \n"
+        " _| _| _||_||_ |_   ||_||_|\n"
+        "  ||_  _|  | _||_|  ||_| _|\n"
+        "                           "
+    )
+    assert guessed_scan(input_) == "123456789"
+
+
+def test_guessed_000000051():
+    input_ = (
+        " _     _  _  _  _  _  _    \n"
+        "| || || || || || || ||_   |\n"
+        "|_||_||_||_||_||_||_| _|  |\n"
+        "                           "
+    )
+    assert guessed_scan(input_) == "000000051"
+
+
+def test_guessed_490867715():
+    input_ = (
+        "    _  _  _  _  _  _     _ \n"
+        "|_||_|| ||_||_   |  |  | _ \n"
+        "  | _||_||_||_|  |  |  | _|\n"
+        "                           "
+    )
+    assert guessed_scan(input_) == "490867715"
 
 
 import ocr
@@ -160,12 +280,11 @@ input_valid = (
     "                           "
 )
 
-valid_number = ocr.Number(input_valid)
-
+valid_number = ocr.Number.from_text(input_valid)
 input_invalid = (
     "    _  _     _  _  _  _  _ \n"
     "  | _| _||_| _ |_   ||_||_|\n"
     "  ||_  _|  | _||_|  ||_| _ \n"
     "                           "
 )
-invalid_number = ocr.Number(input_invalid)
+invalid_number = ocr.Number.from_text(input_invalid)
